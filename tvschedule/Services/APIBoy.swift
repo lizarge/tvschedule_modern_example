72aa114c2748a464a360e3f690fd6d1d7f95c3f0
ankudinov aleksander <sasha@ankudinov.org.ua>
2023-02-13 15:57:31 +0200

    - refactoring

---
+++ tvschedule/Services/APIBoy.swift
@@ -0,0 +1,25 @@
+//
+//  APIBoy.swift
+//  tvschedule
+//
+//  Created by ankudinov aleksandr on 13.02.2023.
+//
+
+import Foundation
+import Alamofire
+
+class APIBoy {
+    
+    //TODO: Cache?
+    
+    private var headers: HTTPHeaders = ["Content-Type":"application/json"]
+    
+    func getChannels() async -> [Channel]? {
+        return (try? await AF.request(Api.сhannelsUrl,method: .get, headers: self.headers).asyncDecodable(of: [Channel].self))
+    }
+    
+    func getProgramItems() async -> [ProgramItem]? {
+        return (try? await AF.request(Api.programItemsUrl,method: .get, headers: self.headers).asyncDecodable(of: [ProgramItem].self))
+    }
+    
+}
---
+++ tvschedule/Services/Models/Channel.swift
@@ -0,0 +1,21 @@
+//
+//  Shedules.swift
+//  tvschedule
+//
+//  Created by ankudinov aleksandr on 13.02.2023.
+//
+
+import Foundation
+
+struct Channel:Codable {
+    let id:Int
+    let orderNum:Int
+    let accessNum:Int
+    let callSign:String
+
+    enum CodingKeys: String, CodingKey {
+        case callSign = "CallSign"
+        case id, orderNum, accessNum
+    }
+}
+
